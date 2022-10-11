97973b9c96259253e94d143a0eda98600642cf8d
Thomas Zwick <thomas.philipp.zwick@gmail.com>
2022-10-11 23:03:48 +0200

    #135/#136

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBoQiVJ65Lrv_-deCm1bAxJ1-KA7sFsyMU",
+    authDomain: "playground-e2cd3.firebaseapp.com",
+    projectId: "playground-e2cd3",
+    storageBucket: "playground-e2cd3.appspot.com",
+    messagingSenderId: "713386247803",
+    appId: "1:713386247803:web:4d9f024c83d6e4b86f04de"
+  };
+
+// init firebase to connect to the system
+firebase.initializeApp(firebaseConfig) 
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp function 
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
