f6e95861e3d5f9ccff73b27775b32d0680969baf
Thomas <thomas.philipp.zwick@gmail.com>
2022-10-13 20:45:01 +0200

    #159

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -1,30 +1,39 @@
-import { ref, watchEffect } from 'vue'
 import { projectStorage } from '../firebase/config'
-import getUser from './getUser'
+import { ref } from "vue"
+import getUser from "./getUser"
 
 const { user } = getUser()
 
 const useStorage = () => {
-    const url = ref(null)
-    const filePath = ref(null)
-    const error = ref(null)
+  const error = ref(null)
+  const url = ref(null)
+  const filePath = ref(null)
 
-    const uploadImage = async (file) => {
-        filePath.value = `covers/${user.value.uid}/${file.name}`
+  const uploadImage = async (file) => {
+    filePath.value = `covers/${user.value.uid}/${file.name}`
+    const storageRef = projectStorage.ref(filePath.value)
 
-        const storageRef = projectStorage.ref(filePath.value)
-
-        try {
-            const res = await storageRef.put(file)
-            url.value = await res.ref.getDownloadURL()
-            error.value = null
-        } catch (err) {
-            console.log(err.message)
-            error.value = err.message
-        }
+    try {
+      const res = await storageRef.put(file)
+      url.value = await res.ref.getDownloadURL()
+    } catch (err) {
+      console.log(err.message)
+      error.value = err
     }
+  }
 
-    return { url, filePath, error, uploadImage }
+  const deleteImage = async (path) => {
+    const storageRef = projectStorage.ref(path);
+
+    try {
+      await storageRef.delete()
+    } catch (err) {
+      console.log(err.message)
+      error.value = err
+    }
+  }
+
+  return { uploadImage, deleteImage, url, filePath, error }
 }
 
-export default useStorage
+export default useStorage;
